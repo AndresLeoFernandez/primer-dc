ef4c84f8d08937782bfc7bd98bab02ee868a7f9f
AndresLeoFernandez <andres.fernandez@municipiodetandil.edu.ar>
2023-11-16 20:26:51 -0300

    clear update project dto

--- src/project/dto/update-project.dto.ts
+++ src/project/dto/update-project.dto.ts
@@ -1,26 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { CreateProjectDto } from './create-project.dto';
 
-/*import { Type } from "class-transformer";
-import { Collaborator } from "src/collaborator/entities/collaborator.entity";
-import { CreateDocumentDto } from "src/document/dto/create-document.dto";
-import { CreateCollaboratorDto } from "src/collaborator/dto/create-collaborator.dto"; */
-
-
-export class UpdateProjectDto extends PartialType(CreateProjectDto) {
-    /*@ApiPropertyOptional({  type:[Collaborator],
-                            required: false,
-                            example: '',
-                            description: 'List of collaborators in the project.'
-    })
-    @Type(()=>Collaborator)
-    @ValidateNested({ each: true })
-    collaborators?: CreateCollaboratorDto[];*/
-
-    /*@ApiPropertyOptional({required: false, example:'158', description: 'Categories Ids of the Users Collaborators.'})
-    @IsArray({ each:true })
-    @IsNumber()
-    @IsOptional()
-    collaborators?: number[];*/
-}
+export class UpdateProjectDto extends PartialType(CreateProjectDto) {}
